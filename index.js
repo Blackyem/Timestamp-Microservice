59917ed1b76ed7de12e13e4f0a43eba13858474b
yelpcamp-profile <kolayem44@gmail.com>
2022-10-19 13:56:42 +0100

    update

--- index.js
+++ index.js
@@ -30,15 +30,36 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 const resObj = {};
 
-app.get("/api/timestamp/:input", (req, res, next) => {
-  const input = req.params.input;
+app.get("/api/timestamp/:input", (req, res) => {
+  let input = req.params.input;
 
   if (input.includes("-")) {
+
+    resObj["unix"] = new Date(input).getTime();
+    resObj["utc"] = new Date(input).toUTCString();
+  } else {
+    input = parseInt(input);
+
     resObj["unix"] = new Date(input).getTime();
     resObj["utc"] = new Date(input).toUTCString();
   }
+  if (!resObj["unix"] || !resObj["utc"]) {
+    res.json({
+      error: "Invalid Date"
+    });
+
+  }
   res.json(resObj);
 });
+
+app.get("/api/timestamp", (req, res) => {
+  resObj["unix"] = new Date().getTime();
+  resObj["utc"] = new Date().toUTCString();
+
+  res.json(resObj)
+});
+
+
 
 
 
